213fcab0d2ffe0a530937d94d51953f8168108fe
Guido Schmechel <guido@ayacoo.de>
2026-04-24 20:00:06 +0200

    Update TYPO3 version

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '8.2.0-8.5.99',
-            'typo3' => '14.0.0-14.3.99',
+            'typo3' => '14.3.0-14.3.99',
         ],
         'conflicts' => [
         ],
